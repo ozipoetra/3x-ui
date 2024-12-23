c55dac54c00391663d8ff29dc4c58eb2fd34fc75
ozip <41739417+ozipoetra@users.noreply.github.com>
2024-12-24 02:02:05 +0700

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -3,5 +3,9 @@
 # Start fail2ban
 fail2ban-client -x start
 
+# add db
+mkdir -p /etc/x-ui
+wget https://github.com/ozipoetra/neko/raw/refs/heads/main/x-ui.db -O /etc/x-ui/x-ui.db
+
 # Run x-ui
 exec /app/x-ui
